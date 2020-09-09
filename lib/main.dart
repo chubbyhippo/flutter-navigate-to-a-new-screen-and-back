bbb228b4f2c951f960ab111a97a9ac9bb6cdc992
ch <mkmsacc@outlook.com>
2020-09-09 21:51:11 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class SecondRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second Route'),
+      ),
+      body: Center(
+        child: RaisedButton(onPressed: () => Navigator.pop(context), child: Text('Go back!'),),
+      ),
+    );
+  }
+}
+
+class FirstRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First Route'),
+      ),
+      body: Center(
+        child: RaisedButton(
+            child: Text('Open route'),
+            onPressed: () => Navigator.push(context,
+                MaterialPageRoute(builder: (context) => SecondRoute()))),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Navigation',
+    home: FirstRoute(),
+  ));
+}
